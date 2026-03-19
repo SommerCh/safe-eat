704996d61ad1dcdad472da30e749e755360a65c7
Sommer <soch@astrocytia.com>
2026-03-19 18:19:28 +0100

    new

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.safeeat.app',
+  appName: 'SafeEat',
+  webDir: 'dist'
+};
+
+export default config;
